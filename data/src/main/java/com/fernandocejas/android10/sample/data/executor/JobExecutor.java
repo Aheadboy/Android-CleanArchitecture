8715060e1b0823635fa3dd79dc3657e71e1737ec
pure <502964379@qq.com>
2021-01-28 17:14:59 +0800

    ljj-comment

--- data/src/main/java/com/fernandocejas/android10/sample/data/executor/JobExecutor.java
+++ data/src/main/java/com/fernandocejas/android10/sample/data/executor/JobExecutor.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 Fernando Cejas Open Source Project
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@ package com.fernandocejas.android10.sample.data.executor;
 
 import android.support.annotation.NonNull;
 import com.fernandocejas.android10.sample.domain.executor.ThreadExecutor;
+
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -28,26 +29,31 @@ import javax.inject.Singleton;
  * Decorated {@link java.util.concurrent.ThreadPoolExecutor} //装饰器模式
  *
  * ljj:
- * 装饰器模式，装饰了{@link java.util.concurrent.ThreadPoolExecutor}
+ * 装饰了{@link java.util.concurrent.ThreadPoolExecutor}
+ * 不是标准的装饰器模式
  */
 @Singleton
 public class JobExecutor implements ThreadExecutor {
-  private final ThreadPoolExecutor threadPoolExecutor;
+    private final ThreadPoolExecutor threadPoolExecutor;
 
-  @Inject
-  JobExecutor() {
-    this.threadPoolExecutor = new ThreadPoolExecutor(3, 5, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new JobThreadFactory());
-  }
-
-  @Override public void execute(@NonNull Runnable runnable) {
-    this.threadPoolExecutor.execute(runnable);
-  }
-
-  private static class JobThreadFactory implements ThreadFactory {
-    private int counter = 0;
-
-    @Override public Thread newThread(@NonNull Runnable runnable) {
-      return new Thread(runnable, "android_" + counter++);
+    @Inject
+    JobExecutor() {
+        this.threadPoolExecutor = new ThreadPoolExecutor(
+                3, 5, 10,
+                TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new JobThreadFactory());
     }
-  }
+
+    @Override
+    public void execute(@NonNull Runnable runnable) {
+        this.threadPoolExecutor.execute(runnable);
+    }
+
+    private static class JobThreadFactory implements ThreadFactory {
+        private int counter = 0;
+
+        @Override
+        public Thread newThread(@NonNull Runnable runnable) {
+            return new Thread(runnable, "android_" + counter++);
+        }
+    }
 }
